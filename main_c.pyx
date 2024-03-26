0d3b34d1a96d10e0e0ca3a10c4500e38cb544fbd
jorge <jresino@gmail.como>
2024-03-26 14:35:18 +0100

    first

---
+++ main_c.pyx
@@ -0,0 +1,339 @@
+
+
+import time
+
+import sys
+
+import cython
+
+sys.setrecursionlimit(10000)  # Ajustar el límite de profundidad de la recursión
+
+
+
+
+def calculate_primes_optimized(amount):
+
+    primes = []
+
+    number  = 2
+
+    found  = 0
+
+    while found < amount:
+
+        for x in primes:
+
+            if number % x == 0:
+
+                break
+
+        else:
+
+            primes.append(number)
+
+            found += 1
+
+        number += 1
+
+    return primes
+
+ 
+
+def calculate_primes(amount):
+
+    primes = []
+
+    number  = 2
+
+    found  = 0
+
+    while found < amount:
+
+        for x in primes:
+
+            if number % x == 0:
+
+                break
+
+        else:
+
+            primes.append(number)
+
+            found += 1
+
+        number += 1
+
+    return primes
+
+ 
+
+@cython.boundscheck(True)
+
+@cython.cdivision(True)
+
+@cython.wraparound(True)
+
+@cython.nonecheck(True)
+
+def calculate_primes_c_wrapped(int amount):
+
+   
+
+    cdef int number
+
+    cdef int found
+
+    cdef int x
+
+   
+
+    cdef int primes[100000]
+
+   
+
+    amount = min(amount,100000)
+
+    number  = 2
+
+    found = 0
+
+   
+
+    while found < amount:
+
+        for x in primes[:found]:
+
+            if number % x == 0:
+
+                break
+
+        else:
+
+            primes[found] = (number)
+
+            found += 1
+
+        number += 1
+
+       
+
+    return [p for p in primes[:found]]
+
+ 
+
+def calculate_primes_c(int amount):
+
+   
+
+    cdef int number
+
+    cdef int found
+
+    cdef int x
+
+   
+
+    cdef int primes[100000]
+
+   
+
+    amount = min(amount,100000)
+
+    number  = 2
+
+    found = 0
+
+   
+
+    while found < amount:
+
+        for x in primes[:found]:
+
+            if number % x == 0:
+
+                break
+
+        else:
+
+            primes[found] = (number)
+
+            found += 1
+
+        number += 1
+
+       
+
+    return [p for p in primes[:found]]
+
+
+
+
+@cython.boundscheck(True)
+
+@cython.cdivision(True)
+
+@cython.wraparound(True)
+
+@cython.nonecheck(True)
+
+cpdef calculate_primes_c_static_wrapped(int amount):
+
+   
+
+    cdef int number
+
+    cdef int found
+
+    cdef int x
+
+   
+
+    cdef int primes[100000]
+
+   
+
+    amount = min(amount,100000)
+
+    number  = 2
+
+    found = 0
+
+   
+
+    while found < amount:
+
+        for x in primes[:found]:
+
+            if number % x == 0:
+
+                break
+
+        else:
+
+            primes[found] = (number)
+
+            found += 1
+
+        number += 1
+
+       
+
+    return [p for p in primes[:found]]
+
+
+
+
+cpdef calculate_primes_c_static(int amount):
+
+   
+
+    cdef int number
+
+    cdef int found
+
+    cdef int x
+
+   
+
+    cdef int primes[100000]
+
+   
+
+    amount = min(amount,100000)
+
+    number  = 2
+
+    found = 0
+
+   
+
+    while found < amount:
+
+        for x in primes[:found]:
+
+            if number % x == 0:
+
+                break
+
+        else:
+
+            primes[found] = (number)
+
+            found += 1
+
+        number += 1
+
+       
+
+    return [p for p in primes[:found]]
+
+
+
+
+
+def fibonacci_recursive_c(int n):
+
+    if n < 2:
+
+        return n
+
+    return fibonacci_recursive_c(n-1) +  fibonacci_recursive_c(n - 2)
+
+ 
+
+def fibonacci_recursive(n):
+
+    if n < 2:
+
+        return n
+
+    return fibonacci_recursive(n-1) +  fibonacci_recursive(n - 2)
+
+
+
+
+cpdef fibonacci_recursive_c_st(n):
+
+    if n < 2:
+
+        return n
+
+    return fibonacci_recursive_c_st(n-1) +  fibonacci_recursive_c_st(n - 2)
+
+
+
+
+
+@cython.boundscheck(True)
+
+@cython.cdivision(True)
+
+@cython.wraparound(True)
+
+@cython.nonecheck(True)
+
+def fibonacci_recursive_c_wrapped(int n):
+
+    if n < 2:
+
+        return n
+
+    return fibonacci_recursive_c_wrapped(n-1) +  fibonacci_recursive_c_wrapped(n - 2)
+
+ 
+
+@cython.boundscheck(True)
+
+@cython.cdivision(True)
+
+@cython.wraparound(True)
+
+@cython.nonecheck(True)
+
+cpdef fibonacci_recursive_c_st_wrapped(int n):
+
+    if n < 2:
+
+        return n
+
+    return fibonacci_recursive_c_wrapped(n-1) +  fibonacci_recursive_c_wrapped(n - 2)
---
+++ test.py
@@ -0,0 +1,124 @@
+import main_c #from main_c_static import calculate_primes_c
+
+import time
+
+
+
+
+if __name__ == '__main__':
+
+   
+
+    """
+
+    start_execution = time.time()
+
+    main_c.calculate_primes(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+    start_execution = time.time()
+
+    main_c.calculate_primes_c(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+ 
+
+    start_execution = time.time()
+
+    main_c.calculate_primes_c_wrapped(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+    start_execution = time.time()
+
+    main_c.calculate_primes_c_static(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+   
+
+    start_execution = time.time()
+
+    main_c.calculate_primes_c_static_wrapped(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+    """
+
+   
+
+    #-------
+
+   
+
+   
+
+    start_execution = time.time()
+
+    main_c.fibonacci_recursive(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+    start_execution = time.time()
+
+    main_c.fibonacci_recursive_c(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+    start_execution = time.time()
+
+    main_c.fibonacci_recursive_c_st(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+    start_execution = time.time()
+
+    main_c.fibonacci_recursive_c_wrapped(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+   
+
+   
+
+    start_execution = time.time()
+
+    main_c.fibonacci_recursive_c_st_wrapped(100000)
+
+    end_execution = time.time()
+
+    print(end_execution-start_execution)
+
+ 
